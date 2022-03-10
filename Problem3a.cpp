544335a3f9f391d40790a8e2492837a06ca83778
Stefan Lutschg <hirzli2@gmail.com>
2022-03-09 21:53:10 -0500

    Homework 3 Code

--- Problem3a.cpp
+++ Problem3a.cpp
@@ -1,10 +1,29 @@
 // your includes here
 #include <vector>
 #include <string>
+#include <iostream>
+
 // countOOO computes the number of pairs of elements in rankedListA that are out of order in rankedListB
 int countOOO(std::vector<std::string> rankedListA, std::vector<std::string> rankedListB) {
 
 	// your implementation goes here
+	int count = 0;
+	int numSongs = rankedListA.size();
+	for(int i = 0; i < numSongs; i++)
+	{
+		for(int j = 0; j<numSongs;j++)
+		{
+			if(rankedListA[i] == rankedListB[j] && i != j) count++;
+		}
+	}
+	return count; // don't forget to change this
+}
 
-	return 0; // don't forget to change this
+
+int main()
+{
+	std::vector<std::string> rankedListA = {"E","G","S"};
+	std::vector<std::string> rankedListB = {"S","E","G"};
+	std::cout << countOOO(rankedListA,rankedListB) << '\n';
+	return 0;
 }
--- Problem3b.cpp
+++ Problem3b.cpp
@@ -1,9 +1,55 @@
 #include "ListNode.h"
 // your includes here
-
+#include <iostream>
+using namespace std;
 DListNode* twowayBubble(DListNode* head) {
 
 	// your implementation goes here
-
-	return NULL; // don't forget to change this
+	DListNode* curr = new DListNode(head->val);
+	//DListNode* tempnode;
+	curr->next = head->next;
+	int min, max = 0, temp;
+	while(curr != NULL) //identifying max & mins
+	{
+		if(curr->val < min) min = curr->val;
+		else if(curr->val > max) max = curr->val;
+		curr = curr->next;
+	}
+	curr = head; //reset
+	while(curr->next != nullptr) // Forward pass
+	{
+	    curr->prev = curr;
+		curr = curr->next;
+		if(curr->val != max)
+		{
+			continue;
+		} 
+		temp = curr->next->val;
+		curr->next->val = max;
+		curr->val = temp;
+		
+	}
+	//curr = curr->prev;
+	while(curr != NULL) // Backwards pass
+	{
+		if(curr->val != min)
+		{
+			curr = curr->prev;
+			continue;
+		} 
+		temp = curr->prev->val;
+		curr->prev->val = min;
+		curr->val = temp;
+		curr = curr->prev;
+	}
+	return head; // don't forget to change this
+}
+int main()
+{
+    DListNode* head = new DListNode(5);
+    head->next = new DListNode(3);
+    head->next->next = new DListNode(2);
+    head->next->next->next = new DListNode(1);
+    DListNode* result = twowayBubble(head);
+    return 0;
 }
